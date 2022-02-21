934e545e648818f9e09e946c81a863e87a359aab
amayaa09 <100150706+amayaa09@users.noreply.github.com>
2022-02-21 11:12:12 -0500

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,15 @@
+num 1= float(input("ingrese un nuemro: "))
+num 2= float(input("ingrese un numero: "))             
+
+if  num_1 == num2:
+    print("son iguales")
+    
+    
+    
+elif num_1  >= num_2:
+    print("el mayor es", num_1)
+    
+    
+elif num_1 <= num_2:
+    print("el mayor es", num_2)
+    
